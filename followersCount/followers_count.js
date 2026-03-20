8d07dd97d0afee1486f915a9964cdae2d79f1270
hsetpainghtoo <hsetpainghtoo218@gmail.com>
2026-03-20 12:21:24 -0400

    add reset button and function

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -6,12 +6,20 @@ function increaseCount() {
   checkCountValue(); // Check count value and display messages
 }
 
+function resetCount() {
+    count = 0;
+    displayCount();
+    checkCountValue();
+}
+
 function displayCount() {
 document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
 }
 
 function checkCountValue() {
-  if (count === 10) {
+    if (count === 0) {
+        alert("Your Instagram Follwers has been reset!");
+      } else if (count === 10) {
     alert("Your Instagram post gained 10 followers! Congratulations!");
   } else if (count === 20) {
     alert("Your Instagram post gained 20 followers! Keep it up!");
