70e74c9f2900daa38a60be6f59a0c08c154b471e
hsetpainghtoo <hsetpainghtoo218@gmail.com>
2026-03-21 11:52:32 -0400

    Add experience textarea

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -10,6 +10,8 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experience = document.getElementById('experienceText').value;
+
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
     document.getElementById('userEmail').innerHTML = email;
@@ -17,6 +19,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = experience;
     alert('Thank you for your valuable feedback')
     document.getElementById('userInfo').style.display = 'block';
 }
