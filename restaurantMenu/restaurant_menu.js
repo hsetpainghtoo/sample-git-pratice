f7b7b0df36ab8e98e8084ee2852e496d7fbe87cd
hsetpainghtoo <hsetpainghtoo218@gmail.com>
2026-03-21 13:09:44 -0400

    Add restaurant menu

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,16 @@
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak- $23', 'Pasta- $11', 'Burger- $29', 'Salmon- $39'];
+const dessertMenu = ['Cake- $11', 'Ice Cream- $9', 'Pudding- $12', 'Fruit Salad- $16'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
