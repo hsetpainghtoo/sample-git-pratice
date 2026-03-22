800392713a10753709df715dfb2275d272baacfe
hsetpainghtoo <hsetpainghtoo218@gmail.com>
2026-03-22 09:50:06 -0400

    Add filter by javascript specialiation

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: "JavaScript" },
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: "Python" },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: "Java" },
       //... More employee records can be added here
     ];
 
@@ -22,6 +22,12 @@ function displayHREmployees() {
      const hrEmployees = employees.filter(employee => employee.department === 'HR');
       const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
       document.getElementById('employeesDetails').innerHTML = hrEmployeesDisplay;
+}
+
+function displayBySpecialization() {
+     const specialization = employees.filter(employee => employee.specialization === 'JavaScript');
+      const specializationDisplay = specialization.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+      document.getElementById('employeesDetails').innerHTML = specializationDisplay;
 }
 
 function findEmployeeById(employeeId) {
